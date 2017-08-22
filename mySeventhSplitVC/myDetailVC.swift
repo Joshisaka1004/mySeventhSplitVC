2c2d0c28b3f8160ed7d205843733f82f2ecf9df1
Joshisaka1004 <joshi1004@gmail.com>
2017-08-22 22:21:03 +0200

    Custom Class for Master and Detail VC

---
+++ mySeventhSplitVC/myDetailVC.swift
@@ -0,0 +1,18 @@
+//
+//  myDetailVC.swift
+//  mySeventhSplitVC
+//
+//  Created by Joachim Vetter on 22.08.17.
+//  Copyright © 2017 Joachim Vetter. All rights reserved.
+//
+
+import UIKit
+
+class myDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ mySeventhSplitVC/myMasterVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  mySeventhSplitVC
+//
+//  Created by Joachim Vetter on 22.08.17.
+//  Copyright © 2017 Joachim Vetter. All rights reserved.
+//
+
+import UIKit
+
+class myMasterVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
